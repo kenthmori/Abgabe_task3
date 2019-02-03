66f36ebf92b2aafd55fd61d03203e75402ce123b
kenthmori <ken.tha.mori@gmail.com>
2019-02-03 10:00:44 +0100

    commited the files in directory Ex1

---
+++ Ex1/welcome.c
@@ -0,0 +1,7 @@
+#include "welcome.h"
+#include <stdio.h>
+
+void hello()
+{
+    printf("Hello from SWP Vorlesung\n");
+}
---
+++ Ex1/welcome.h
@@ -0,0 +1,7 @@
+#ifndef WELCOME_H
+#define WELCOME_H
+
+void hello();
+
+#endif //WELCOME_H
+
